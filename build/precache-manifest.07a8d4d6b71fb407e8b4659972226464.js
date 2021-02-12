2dcc261a7cba690cb151b71ca28f58595e551c20
Philip Krogel <pkrogel@krogelwebsolutions.com>
2021-02-12 13:22:37 -0500

    forgot to build

---
+++ build/precache-manifest.07a8d4d6b71fb407e8b4659972226464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4671755487f687c2712a268f572bfa56",
+    "url": "/Tutorials/Mirrors/index.html"
+  },
+  {
+    "revision": "addab6273720fdd8e4c5",
+    "url": "/Tutorials/Mirrors/static/css/main.3e0da3ba.chunk.css"
+  },
+  {
+    "revision": "5932b593f9735664e549",
+    "url": "/Tutorials/Mirrors/static/js/2.9816361c.chunk.js"
+  },
+  {
+    "revision": "4a39ab3be31c11d2dea3d04487e78b26",
+    "url": "/Tutorials/Mirrors/static/js/2.9816361c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "addab6273720fdd8e4c5",
+    "url": "/Tutorials/Mirrors/static/js/main.afe5ae61.chunk.js"
+  },
+  {
+    "revision": "7c954e1fe14189945e1a",
+    "url": "/Tutorials/Mirrors/static/js/runtime-main.3f5545e1.js"
+  }
+]);
