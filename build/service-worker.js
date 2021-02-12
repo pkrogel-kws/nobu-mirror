16a831f69e90e11995867d5c8d54e48f94269d79
Philip Krogel <pkrogel@krogelwebsolutions.com>
2021-02-12 14:32:36 -0500

    try fix barba react transitions

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Tutorials/Mirrors/precache-manifest.3b0a54865490f3e02acab448baea2f08.js"
+  "/Tutorials/Mirrors/precache-manifest.797071dc6c29c1c51cd8b87008e6bb55.js"
 );
 
 self.addEventListener('message', (event) => {
